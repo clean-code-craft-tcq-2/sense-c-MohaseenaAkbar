9ffae580cce2e2093c26448651edec6d07b41f3a
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2021-10-24 19:28:56 +0530

    Update stats.h
    Update extern declaration

--- stats.h
+++ stats.h
@@ -11,3 +11,5 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stat
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
+extern int emailAlerter;
+extern int ledAlerter;
