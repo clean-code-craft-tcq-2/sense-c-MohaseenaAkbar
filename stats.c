11cf5d76a252923c3e6b3f774d1cdc05166a1e19
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2021-10-24 19:29:34 +0530

    Update stats.c
    update alerter function

--- stats.c
+++ stats.c
@@ -15,6 +15,8 @@ return s;
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
+int emailAlerter = 0;
+int ledAlerter = 0;
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
