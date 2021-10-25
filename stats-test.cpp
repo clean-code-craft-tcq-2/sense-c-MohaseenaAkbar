220db75e3bfffcf836aedf8878ed029b870c9a43
MohaseenaAkbar <91214072+MohaseenaAkbar@users.noreply.github.com>
2021-10-25 13:00:07 +0530

    Update stats-test.cpp
    Final update

--- stats-test.cpp
+++ stats-test.cpp
@@ -21,20 +21,14 @@ TEST_CASE("average is NaN for empty array") {
     int setlength = 0;
     bool averge;
     struct Stats computedStats = compute_statistics(numberset, setlength);
-    //All fields of computedStats (average, max, min) must be
-    //NAN (not-a-number), as defined in math.h
     
-    //Design the REQUIRE statement here.
     averge = isnan(computedStats.average)?true:false;
     REQUIRE(averge == true);
     REQUIRE(abs(computedStats.max == 0));
     REQUIRE(abs(computedStats.min == 0));
-    //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
 }
 
 TEST_CASE("raises alerts when max is greater than threshold") {
-    // create additional .c and .h files
-    // containing the emailAlerter, ledAlerter functions
     alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
 
     float numberset[] = {99.8, 34.2, 4.5};
@@ -44,8 +38,6 @@ TEST_CASE("raises alerts when max is greater than threshold") {
     const float maxThreshold = 10.2;
     check_and_alert(maxThreshold, alerters, computedStats);
 
-    // need a way to check if both emailAlerter, ledAlerter were called
-    // you can define call-counters along with the functions, as shown below
     REQUIRE(emailAlertCallCount == 1);
     REQUIRE(ledAlertCallCount == 1);
 }
